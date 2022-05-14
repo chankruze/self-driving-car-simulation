fd4db45b040afe12d47f129bc713733be93605c2
chankruze <chankruze@gmail.com>
2022-05-14 09:19:25 +0530

    basic canvas setup
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+Author: chankruze (chankruze@gmail.com)
+Created: Sat May 14 2022 01:00:59 GMT+0530 (India Standard Time)
+
+Copyright (c) geekofia 2022 and beyond
+*/
+
+const canvas = document.getElementById("canvas");
+// set height and width of canvas
+canvas.width = 200;
+canvas.height = window.innerHeight;
+
+// canvas context
+const ctx = canvas.getContext("2d");
