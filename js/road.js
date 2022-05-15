622ee2b3e0f150cf083466e07a3af72122f80ccd
chankruze <chankruze@gmail.com>
2022-05-15 23:04:35 +0530

    consistent structure for drawing lanes
    Signed-off-by: chankruze <chankruze@gmail.com>

--- js/road.js
+++ js/road.js
@@ -17,6 +17,16 @@ class Road {
     const INFINITY = 10000000;
     this.top = -INFINITY; // y-axis is negative
     this.bottom = INFINITY;
+
+    const topLeft = { x: this.left, y: this.top };
+    const topRight = { x: this.right, y: this.top };
+    const bottomLeft = { x: this.left, y: this.bottom };
+    const bottomRight = { x: this.right, y: this.bottom };
+
+    this.borders = [
+      [topLeft, bottomLeft],
+      [topRight, bottomRight],
+    ];
   }
 
   getLaneCenter(laneIndex) {
@@ -31,22 +41,25 @@ class Road {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "#fff";
 
-    // loop laneCount + 1 times to draw lane edges
-    for (let index = 0; index <= this.laneCount; index++) {
+    // draw the inner edges dashed lane edges
+    for (let index = 1; index < this.laneCount; index++) {
+      // get the x-coordinate of the edge of the lane
       const lenX = lerp(this.left, this.right, index / this.laneCount);
-
-      // dashed line for inner lane edges
-      if (index > 0 && index < this.laneCount) {
-        ctx.setLineDash([30, 15]);
-      } else {
-        ctx.setLineDash([]);
-      }
-
-      // draw the lane edges
+      // draw the inner edges
+      ctx.setLineDash([30, 15]);
       ctx.beginPath();
       ctx.moveTo(lenX, this.top);
       ctx.lineTo(lenX, this.bottom);
       ctx.stroke();
     }
+
+    // draw the borders
+    ctx.setLineDash([]);
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.moveTo(border[0].x, border[0].y);
+      ctx.lineTo(border[1].x, border[1].y);
+      ctx.stroke();
+    });
   }
 }
